ce0ea35d96fc51fff800bd0c628d1d673e9955a7
Anthony Economou <anthony.economou@avanade.com>
2020-10-03 00:44:07 +0900

    Added html file and client.js file.

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+// serve the homepage
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.sendFile(__dirname + '/index.html');
+  });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening on ${port}`)
 })
