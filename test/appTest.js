accc137e95b93777584141b9c1779f678a016ee3
Anthony Economou <anthony.economou@avanade.com>
2020-10-03 22:45:43 +0900

    Added mocha & chai testing suite to project.

---
+++ test/appTest.js
@@ -0,0 +1,19 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+chai.use(chaiHttp);
+
+var server = require('../app');
+
+describe('Webpage root load', () => {
+  it('it should load default page', (done) => {
+    chai.request(server)
+    .get('/')
+    .end((err, res) => {
+     // var result = res.statusCode
+      expect(res.status).to.equal(401);
+      done();
+    });
+  })
+});
