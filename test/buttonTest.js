3f317f7138f9dd4f7544e05e9200768bf07bdec0
Anthony Economou <anthony.economou@avanade.com>
2020-10-07 06:55:25 +0900

    Updated code with comments. Updated exit code conditions for buttonTest.js.

--- test/buttonTest.js
+++ test/buttonTest.js
@@ -1,64 +1,64 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//Name: buttontest.js
+//Author: Anthony Economou
+//Date: 2020-10-05
+//Description: Used to test webpage button functionality.
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//declare variables
 var webdriver = require('selenium-webdriver');
 var server = require('../app');
 var chrome = require('selenium-webdriver/chrome');
 var assert = require('assert');
-
 var driver;
 var By = webdriver.By,
 until = webdriver.until;
 
+//startWebsite function:kicks off the webdriver to run the webpage
 function startWebsite() {
   driver = new webdriver.Builder()
   .forBrowser('chrome')
   .setChromeOptions(new chrome.Options().addArguments("start-maximized"))
-  //  .add_experimental_option('excludeSwitches', ['enable-logging'])
   .build();
   driver.get('http://localhost:3000');
   
 };
 
+//testing function: used to check the text element's text content before button press.
 function testing(callback) {
-  var prom1 = driver.findElement(By.id('text1')).getText().then(function(el){
+  driver.findElement(By.id('text1')).getText().then(function(el){
     console.log("[Before] text1: " + el);
     textBefore = el;
   });
   var button1 = driver.findElement(By.id('Button1')).getText().then(function(el){
-    //console.log("button1 text: " + el);
   });
-  // driver.wait(until.elementLocated(By.id('text1')));
+
+//simulate button click.
   driver.findElement(By.id('Button1')).click();
-  //button1.click();
-  
   console.log('Button clicked.');
-  var prom2 = driver.findElement(By.id('text1')).getText().then(function(el){
+
+  //re-check the text element for updated text content.
+  driver.findElement(By.id('text1')).getText().then(function(el){
   console.log("[After] text1: " + el);
   textAfter = el;
-  //assert(textBefore === textAfter);
+  
+//compare the text content of the webpage before and after the button click, if they are different = test is successful!
   try{
-    //assert('1' === '1');
-    assert(textBefore !== textAfter);
-    console.log('TEST PASSED: Text changed after button press.');
+    assert(textBefore !== textAfter); //button updated the text element's text - test successful
+    console.log('TEST PASSED: Text changed after button press.'); 
   }
   catch{
-    console.log('TEST FAILED: Text did not changed after button press.');
+    console.log('TEST FAILED: Text did not changed after button press.'); // text element's text content has not changed - test failure
+    process.exit(1);
   }
   });
 }
-
-//testing();
-
+//endProgram function: ends nodejs program with a return code (0).
 function endProgram(number, callback) {
-  // driver.wait(function() {
-  //   return driver.executeScript('return document.readyState').then(function(readyState) {
-  //     return readyState === 'complete';
-  //   });
-  // });
-    process.exit(1);
+    process.exit(0);
 };
 
+//main functions called
 startWebsite();
 setTimeout(testing,5000);//waits 5 seconds then executes function inside
 setTimeout(endProgram,7000);
-//endProgram(1,testing(startWebsite()));
-
-//driver.quit();
