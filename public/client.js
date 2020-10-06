8f8834207daa0a4400d3d7820be252cdb1496e60
Anthony Economou <anthony.economou@avanade.com>
2020-10-07 07:14:58 +0900

    Revised the webpage text displayed before and after button click.

--- public/client.js
+++ public/client.js
@@ -1,17 +1,14 @@
-
 console.log('Client-side code running');
 
 const button = document.getElementById('Button1');
 const text = document.getElementById('text1');
 
 function buttonClick() {
-  console.log('button was clicked');
-  text.textContent = 'Text updated.'
+  console.log('The button was clicked');
+  text.textContent = 'The text was updated.'
   return text.textContent
 }
 
 button.addEventListener('click', (e) => {
   buttonClick()
 });
- 
-module.exports = buttonclick()
